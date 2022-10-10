e0a31c0ec161474849c208e65dde3c18875d0702
Freddie Yang <dark_yty@hotmail.com>
2022-10-10 17:54:19 +1100

    added contact and resume

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -1,56 +1,136 @@
-import React from 'react';
-
-
-const handleFormSubmit = (e) => {
-  e.preventDefault();
-
-}
-
+import React, { useState } from 'react';
 
 export default function ContactForm(props) {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name
+    const inputValue = target.value
+
+    if (inputType === 'name') {
+      setName(inputValue);
+    }
+
+    if (inputType === 'email') {
+      setEmail(inputValue); 
+    }
+
+    if (inputType === 'message') {
+      setMessage(inputValue);
+    }
+  };
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      document.querySelector("#name").style.borderColor = "red";
+      document.querySelector("#error-name").innerHTML = "Please enter a valid name";
+      document.querySelector("#error-name").style.color = "red";
+    }  else {
+      document.querySelector("#name").style.borderColor = "#ced4da";
+      document.querySelector("#error-name").innerHTML = "";
+    }
+
+    if (!validateEmail(email)){
+      document.querySelector("#email").style.borderColor = "red";
+      document.querySelector("#error-email").innerHTML = "Please enter a valid email";
+      document.querySelector("#error-email").style.color = "red";
+    } else {
+      document.querySelector("#email").style.borderColor = "#ced4da";
+      document.querySelector("#error-email").innerHTML = "";
+    }
+
+    if (!message) {
+      document.querySelector("#message").style.borderColor = "red";
+      document.querySelector("#error-message").innerHTML = "Please enter a valid message";
+      document.querySelector("#error-message").style.color = "red";
+    } else {
+      document.querySelector("#message").style.borderColor = "#ced4da";
+      document.querySelector("#error-message").innerHTML = "";
+    }
+
+    if (name && message && validateEmail(email)) {
+      window.open(`mailto: freddie.yty@gmail.com?subject=${name}&body=${message}`);
+
+      setName('');
+      setEmail('');
+      setMessage('');
+      alert("email is ready!");
+    }
+
+    
+  }
+
+
   return (
     <div className='container px-4 py-5'>
       <h2 className="pb-2 border-bottom">Contact me</h2>
       <div className="mb-3">
         <label className="form-label">Your name</label>
         <input
-          value={props.value}
+          value={name}
+          name='name'
+          onChange={handleInputChange}
           type="text"
-          class="form-control"
+          className="form-control"
           id="name"
           placeholder="your name"
-          data-toggle="tooltip" 
+          data-toggle="tooltip"
           data-html="true"
-          data-placement="top" 
+          data-placement="top"
           title="Please enter your name here" />
+
       </div>
       <div className="mb-3">
         <label className="form-label">Email address</label>
         <input
-          value={props.value}
+          value={email}
+          onChange={handleInputChange}
+          name='email'
           type="email"
-          class="form-control"
+          className="form-control"
           id="email"
           placeholder="name@example.com"
-          data-toggle="tooltip" 
+          data-toggle="tooltip"
           data-html="true"
-          data-placement="top" 
+          data-placement="top"
           title="Please enter your email here" />
       </div>
       <div className="mb-3">
         <label className="form-label">Your message</label>
         <textarea
-          class="form-control"
-          id="content"
-          value={props.value}
+          className="form-control"
+          id="message"
+          name='message'
+          value={message}
+          onChange={handleInputChange}
           rows="3"
-          data-toggle="tooltip" 
+          data-toggle="tooltip"
           data-html="true"
-          data-placement="top" 
+          data-placement="top"
           title="Please enter your message here"></textarea>
       </div>
-      <div lassName="mb-3">
-        <button onClick={props.handleFormSubmit} type="submit" class="btn btn-primary">Submit</button>
+      <div>
+        <p id='error-name'></p>
+        <p id='error-email'></p>
+        <p id='error-message'></p>
+      </div>
+      <div className="mb-3">
+        <button onClick={handleFormSubmit} type="submit" className="btn btn-primary">Submit</button>
       </div>
     </div>
   );
--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -1,22 +1,44 @@
 import React from 'react';
+import resume from '../../assets/resume.pdf';
+
 
 export default function Resume() {
   return (
     <div className='container px-4 py-5'>
       <h2 className="pb-2 border-bottom">Resume</h2>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <div>
+        <h4>Download Resume <a href={resume}>here</a></h4>
+      </div>
+
+      <div>
+        <h4>Front-end Technology</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>GraphQL</li>
+          <li>Tailwind</li>
+          <li>PWA</li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Back-end Technology</h4>
+        <ul>
+          <li>Node.js</li>
+          <li>Express.js</li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Database Technology</h4>
+        <ul>
+          <li>MySQL</li>
+          <li>mongoDB</li>
+        </ul>
+      </div>
     </div>
   );
 }
+
